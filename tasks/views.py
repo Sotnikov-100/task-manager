cd9bd516012e9a332a185fe747f988e4dc708796
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-04-02 10:17:08 +0300

    add core templates (base, sidebar, index, css) and update urls, views and create urls to tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.utils import timezone
+from tasks.models import Task
 
-# Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        assigned_tasks = Task.objects.filter(
+            assignees=request.user
+        ).order_by("-deadline")[:5]
+
+        total_tasks = Task.objects.filter(assignees=request.user).count()
+        completed_tasks = Task.objects.filter(
+            assignees=request.user,
+            is_completed=True
+        ).count()
+
+        overdue_tasks = Task.objects.filter(
+            assignees=request.user,
+            deadline__lt=timezone.now(),
+            is_completed=False
+        ).count()
+    else:
+        assigned_tasks = []
+        total_tasks = 0
+        completed_tasks = 0
+        overdue_tasks = 0
+
+    context = {
+        "assigned_tasks": assigned_tasks,
+        "total_tasks": total_tasks,
+        "completed_tasks": completed_tasks,
+        "overdue_tasks": overdue_tasks,
+    }
+    return render(request, "tasks/index.html", context)
