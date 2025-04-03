a22bb38d03c8c3c1f86445c3d2f90a590ec8cd2c
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-04-03 13:58:06 +0300

    Overhaul password reset flow with: - updated templates - improved error handling - responsive design fixes and Create directory tests

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from tasks.views import (
+    CustomPasswordChangeView,
     IndexView,
     ProfileView,
     SignUpView,
@@ -45,17 +46,42 @@ urlpatterns = [
     ),
     path(
         "password-change/",
-        auth_views.PasswordChangeView.as_view(
-            template_name="registration/password_change.html"
-        ),
+        CustomPasswordChangeView.as_view(),
         name="password_change"
     ),
     path(
-        "password-change/done/",
-        auth_views.PasswordChangeDoneView.as_view(
-            template_name="registration/password_change_done.html"
-        ),
-        name="password_change_done"
+        "password-reset/",
+         auth_views.PasswordResetView.as_view(
+             template_name="registration/password_reset_form.html",
+             email_template_name="registration/password_reset_email.html",
+             success_url=reverse_lazy("password_reset_done")
+         ),
+         name="password_reset"
+    ),
+
+    path(
+        "password-reset/done/",
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="registration/password_reset_done.html"
+         ),
+         name="password_reset_done"
+    ),
+
+    path(
+        "password-reset/<uidb64>/<token>/",
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="registration/password_reset_confirm.html",
+             success_url=reverse_lazy("password_reset_complete")
+         ),
+         name="password_reset_confirm"
+    ),
+
+    path(
+        "password-reset/complete/",
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="registration/password_reset_complete.html"
+         ),
+         name="password_reset_complete"
     ),
     path(
         "tasks/",
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 from django.db.models import Q
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -132,3 +133,6 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy("tasks:task-list")
 
 
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = "registration/password_change.html"
+    success_url = reverse_lazy("tasks:profile")
