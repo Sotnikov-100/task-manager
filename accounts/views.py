b35327ef593697fcea2392924da42801ec98a24a
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-04-21 11:55:44 +0300

    Refactor registration form styling and implement post-signup auto-login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -8,7 +9,12 @@ from accounts.forms import WorkerCreationForm
 class SignUpView(CreateView):
     form_class = WorkerCreationForm
     template_name = "registration/signup.html"
-    success_url = reverse_lazy("accounts:login")
+    success_url = reverse_lazy("tasks:index")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
 
 
 class CustomPasswordChangeView(PasswordChangeView):
